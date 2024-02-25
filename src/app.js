85366eb464491809edf40c2e6da08055f2ef2ddd
Alessandra198 <rinaldoalessandra@gmail.com>
2024-02-25 22:04:22 +0100

    Added libraries and AI

--- src/app.js
+++ src/app.js
@@ -1,12 +1,25 @@
-function generateTale(event) {
-  event.preventDefault();
-
+function displayTale(response) {
   new Typewriter("#tale", {
-    strings: "Fairy tale",
+    strings: response.data.answer,
     autoStart: true,
     cursor: null,
     delay: 20,
   });
+}
+function generateTale(event) {
+  event.preventDefault();
+  let instruction = document.querySelector("#input-topic");
+  getApi(instruction.value);
+}
+
+function getApi(topic) {
+  let apiKey = "cb4440cc8b49o2e10d5a63f3ecftcafe";
+  let context =
+    "You are a kids' fairy tale expert and love to write short fairy tales. Write a short fairy tale, about 10 lines, in basic HTML inside a <p>. Make sure to follow the user instructions. Add a title and sign the tale at the end inside a <small> and <strong> element with 'Shecodes AI'";
+  let prompt = `User instructios: Generate a Fairy Tale for kids about ${topic}`;
+  let apiURl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiURl).then(displayTale);
 }
 
 let formElement = document.querySelector("#tale-generator-form");
