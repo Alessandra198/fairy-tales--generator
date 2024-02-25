01773261fd4f2cb75e1bb2fd29df7fff87dc83c0
Alessandra198 <rinaldoalessandra@gmail.com>
2024-02-25 22:25:20 +0100

    Improved the User Experience

--- src/app.js
+++ src/app.js
@@ -15,10 +15,13 @@ function generateTale(event) {
 function getApi(topic) {
   let apiKey = "cb4440cc8b49o2e10d5a63f3ecftcafe";
   let context =
-    "You are a kids' fairy tale expert and love to write short fairy tales. Write a short fairy tale, about 10 lines, in basic HTML inside a <p>. Make sure to follow the user instructions. Add a title and sign the tale at the end inside a <small> and <strong> element with 'Shecodes AI'";
+    "You are a kids' fairy tale expert and love to write short fairy tales. Write a short fairy tale, about 10 lines, in basic HTML inside a <p>. Make sure to follow the user instructions. Add a title at the top and sign the tale at the end inside a <small> and <strong> element with 'Shecodes AI'";
   let prompt = `User instructios: Generate a Fairy Tale for kids about ${topic}`;
   let apiURl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
+  let taleElement = document.querySelector("#tale");
+  taleElement.classList.remove("hidden");
+  taleElement.innerHTML = `⏳ Generating a fairy tale about ${topic} for you...`;
   axios.get(apiURl).then(displayTale);
 }
 
